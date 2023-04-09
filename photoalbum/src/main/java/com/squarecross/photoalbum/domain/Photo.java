d36e30e81a9ca007d47af89a01e2682ef82fbe0a
YOUNJAE <116246006+coyojo@users.noreply.github.com>
2023-04-10 00:17:35 +0900

    Implement Photo Domain Class

---
+++ photoalbum/src/main/java/com/squarecross/photoalbum/domain/Photo.java
@@ -0,0 +1,103 @@
+package com.squarecross.photoalbum.domain;
+
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
+
+//@Entity는 DB테이블과 1-1로 매핑되는 클래스 단위로 Entity 객체 하나가 DB하나의 레코드 즉 하나의 ROW를 의미
+@Entity
+@Table(name = "photo", schema="photo_album",uniqueConstraints = {@UniqueConstraint(columnNames = "photo_id")})
+public class Photo {
+
+    //@Id는 해당 Entity의 Primary Key로 사용한다는 의미
+    //@Id는 같은 테이블내애서 중복될 값이 있을 수 없다.
+    //@GenerateValue는 @id 값을 새롭게 부여할 때 사용하는 방법에 대한 정보를 입력
+    //strategy = GenerationType.IDENTITY눈
+    //가장 최근 id에 +1 을 해서 다음 아이디를 생성합니다.
+    //@Column은 album 테이블의 매핑되는 column 정보를 입력
+    //name은 컬럼명, unique는 중복불가, nullable은 null값 허용 여부
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="photo_id", unique = true,nullable = false)
+    private Long photoId;
+
+    @Column(name = "file_name", unique = false,nullable = true)
+    private String fileName;
+
+    @Column(name = "thumb_url", unique = false, nullable = true)
+    private String thumbUrl;
+
+    @Column(name = "original_url", unique = false, nullable = true)
+    private String originalUrl;
+
+    @Column(name = "file_size", unique = false, nullable = true)
+    private int fileSize;
+
+    @Column(name = "uploaded_at", unique = false, nullable = true)
+    @CreationTimestamp
+    private Date uploadedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    public Photo(){};
+
+    public Long getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(Long photoId) {
+        this.photoId = photoId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getThumbUrl() {
+        return thumbUrl;
+    }
+
+    public void setThumbUrl(String thumbUrl) {
+        this.thumbUrl = thumbUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Date getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(Date uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+}
